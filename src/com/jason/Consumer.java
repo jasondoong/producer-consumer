737ad9061b8625f62198b468f6ec3cae12a7e01e
Jason Dong <jasondoong@gmail.com>
2015-01-24 20:24:11 +0800

    add base class for Producer and Consumer

--- src/com/jason/Consumer.java
+++ src/com/jason/Consumer.java
@@ -1,12 +1,5 @@
 package com.jason;
 
-import java.util.concurrent.BlockingQueue;
+public abstract class Consumer <T> extends ProducerConsumerBasic<T> {
 
-public abstract class Consumer <T> implements Runnable{
-
-  protected BlockingQueue<T> queue;
-
-  public void setQueue(BlockingQueue<T> queue) {
-    this.queue = queue;
-  }
 }
--- src/com/jason/Producer.java
+++ src/com/jason/Producer.java
@@ -1,12 +1,5 @@
 package com.jason;
 
-import java.util.concurrent.BlockingQueue;
-
-public abstract class Producer <T> implements Runnable {
-
-  protected BlockingQueue<T> queue;
-  public void setQueue(BlockingQueue<T> q){
-    this.queue=q;
-  }
+public abstract class Producer <T> extends ProducerConsumerBasic<T> {
 
 }
