ac8de2f53f63f4368217b2fa4c462fa036ffb413
Jason Dong <jasondoong@gmail.com>
2015-02-05 11:24:31 +0800

    Move basic consume logic into consumer base class

--- src/main/java/com/client/MyConsumer.java
+++ src/main/java/com/client/MyConsumer.java
@@ -6,20 +6,17 @@ import com.jason.ConsumerElement;
  * Created by jason on 2015/1/24.
  */
 public class MyConsumer extends ConsumerElement<Message> {
+
   @Override
-  public void run() {
-    try{
-      Message msg;
-      //consuming messages until exit message is received
-      while(true){
-        msg = getFromBuffer();
-        System.out.println("                        "+
-          "["+Thread.currentThread().getId()+"]"+
-          " takes "+msg.getMsg());
-        Thread.sleep(1000);
-      }
-    }catch(InterruptedException e) {
-      System.out.println("Consumer stops cause of interrupt");
-    }
+  protected void consume(Message msg) throws InterruptedException {
+    System.out.println("                        "+
+      "["+Thread.currentThread().getId()+"]"+
+      " takes "+msg.getMsg());
+    Thread.sleep(1000);
+  }
+
+  @Override
+  protected void interruptHandler() {
+    System.out.println("Consumer stops cause of interrupt");
   }
 }
--- src/main/java/com/jason/ConsumerElement.java
+++ src/main/java/com/jason/ConsumerElement.java
@@ -1,9 +1,28 @@
 package com.jason;
 
+import com.client.Message;
+
 public abstract class ConsumerElement<T> extends Element<T> {
 
   protected T getFromBuffer() throws InterruptedException {
     return queue.take();
   }
+
+  @Override
+  public void run() {
+    try{
+      //consuming messages until exit message is received
+      while(true){
+        T item = getFromBuffer();
+        consume(item);
+      }
+    }catch(InterruptedException e) {
+      interruptHandler();
+    }
+  }
+
+  protected abstract void consume(T item) throws InterruptedException;
+
+  protected abstract void interruptHandler();
 
 }
